eba5a0547d4d55b7acc7cf041fccc56e931f5790
berkaysson <berkaysonel85@gmail.com>
2024-01-14 13:31:49 +0300

    add mongodb connection middleware

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const { logger } = require("./middleware/logger");
+const { logger, logEvents } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const connectDb = require("./config/dbConnect");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3500;
+
+connectDb();
 
 app.use(logger);
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
-
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
 
@@ -29,4 +33,16 @@ app.all("*", (req, res) => {
 });
 
 app.use(errorHandler);
-app.listen(PORT, () => console.log("Server running on port: " + PORT));
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDb");
+
+  app.listen(PORT, () => console.log("Server running on port: " + PORT));
+});
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
