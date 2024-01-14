00ecefd45488ac2e2942df745cf7a17e8cc0fdf9
berkaysson <berkaysonel85@gmail.com>
2024-01-14 15:28:07 +0300

    implement DashLayout and Public components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import Public from "./components/Public";
+import DashLayout from "./components/DashLayout";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
 
+        <Route path="dash" element={<DashLayout />}>
+
+        </Route>
+        {/* End Dash */}
       </Route>
     </Routes>
   );
